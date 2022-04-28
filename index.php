c4a7eac74f2a99b5cbba453040c17b70fd21310a
Miqueias <miqueias.sousa.ti@gmail.com>
2022-04-28 18:14:34 -0300

    Mudança das pagina para PHP, adicionado o Require

---
+++ index.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/owl/owl.carousel.min.css" />
+  <link rel="stylesheet" href="css/owl/owl.theme.default.min.css" />
+  <link rel="stylesheet" href="css/magnific-popup.css" />
+  <link rel="stylesheet" href="css/main.css" />
+
+  <!-- SCRIPT -->
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"></script>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+
+  <title>Miquéias Sousa</title>
+
+</head>
+
+<body>
+
+  <!-- MENU GERAL -->
+  <div class="menu-geral">
+
+    <!-- HEADER -->
+    <?php require "paginas/header.php"; ?>
+
+    <!-- BODY -->
+
+  </div> <!-- FIM MENU GERAL -->
+
+    <!-- CONTATO -->
+    <?php require "paginas/body.php"; ?>
+
+
+  <div id="contato">
+
+    <!-- CONTATO -->
+    <?php require "paginas/contato.php"; ?>
+
+    <!-- FOOTER -->
+    <?php require "paginas/footer.php"; ?>
+
+  </div>
+  </div>
+</body>
+
+
+<!-- Icones -->
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<script src="js/bootstrap.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/isotope.pkgd.min.js"></script> <!-- ISOTOPE -->
+<script src="js/magnify/jquery.magnific-popup.min.js"></script>
+<script src="js/main.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+
+
+</html>
---
+++ paginas/contato.php
@@ -0,0 +1,88 @@
+<div class="container">
+      <div class="row">
+        <!-- div> <img src="assets/paperplane.svg" /> </div> -->
+
+        <!-- CONTATO COLUNA 1 -->
+        <div class="col-12 col-lg-4 col-xl-4 col-sm-12 col-md-12 mb-0 mt-0 mb-lg-0 order-1">
+          <div class="row coluna1">
+            <div class="row">
+              <h3 class="mb-4">CONTATO</h3>
+              <div class="col-md-1 col-sm-6 col-1"> <i class="simbolo fas fa-map-marker-alt"></i> </div>
+              <div class="col-md-8 col-sm-6 col-8">
+                <p>Localização:</p>
+                <div class="t-opaco"> Vitória da Conquista, BA </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-1 col-1"> <i class="simbolo fas fa-envelope"></i> </div>
+              <div class="col-md-8 col-6">
+                <p>E-mail:</p>
+                <div class="t-opaco"> miqueias.sousa.ti@gmail.com </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-1 col-1"> <i class="simbolo fa-brands fa-whatsapp"></i> </div>
+              <div class="col-md-8 col-6">
+                <p>WhatsApp:</p>
+                <div class="t-opaco"> (73) 98212 7319</div>
+              </div>
+            </div>
+
+            <div class="row">
+              <h3 class="mt-4">REDES SOCIAIS</h3>
+
+              <div class="col-md-1"></div>
+
+
+
+              <div class="list" id="list">
+                <ul class="list">
+                  <li>
+                    <a href="https://contate.me/miqueiassousa" target="_blank" rel="noopener noreferrer"><i
+                        class="fa-brands fa-whatsapp"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://www.instagram.com/miqueiassousateclas/" target="_blank" rel="noopener noreferrer">
+                      <i class="fa-brands fa-instagram"></i></i></a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/in/sousamiqueias/" target="_blank" rel="noopener noreferrer"><i
+                        class="fa-brands fa-linkedin-in"></i></a>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+        <!-- CONTATO COLUNA 2 -->
+        <div class="col-6 col-lg-4 col-xl-4 col-sm-12 col-md-12 mb-0 mt-0 order-2 coluna2"></div>
+
+        <!-- CONTATO COLUNA 3 -->
+        <div class="col-12 col-lg-4 col-xl-4 col-sm-12 col-md-6 order-3">
+          <form class="row coluna3">
+            <h3>FORMULÁRIO DE CONTATO </h3>
+            <div>
+              <label for="exampleFormControlInput1" class="form-label"> </label>
+              <p>Nome:</p>
+              <input type="name" class="bg-light form-control form-control text-dark" id="exampleFormControlInput1">
+            </div>
+            <div>
+              <label for="exampleFormControlInput1" class="form-label"> </label>
+              <p>E-mail:</p>
+              <input type="email" class="bg-light form-control text-dark" id="exampleFormControlInput1">
+            </div>
+            <div>
+              <label for="exampleFormControlTextarea1" class="form-label"></label>
+              <p>Mensagem:</p>
+              <textarea class="form-control bg-light text-dark" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <div class="btn button-area">
+              <button type="submit">Enviar</button>
+            </div>
+          </form>
+        </div>
+      </div>
