40a19cafdd7815636cee6da253094edc7e10eaf9
Miqueias <miqueias.sousa.ti@gmail.com>
2022-04-28 18:44:41 -0300

    Istacao do PIXEL do FB

--- index.php
+++ index.php
@@ -24,6 +24,31 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
 
+  <!-- Meta Pixel Code -->
+  <script>
+    ! function(f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function() {
+        n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s)
+    }(window, document, 'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '298720425109776');
+    fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=298720425109776&ev=PageView&noscript=1" /></noscript>
+  <!-- End Meta Pixel Code -->
 
   <title>Miquéias Sousa</title>
 
@@ -41,8 +66,8 @@
 
   </div> <!-- FIM MENU GERAL -->
 
-    <!-- CONTATO -->
-    <?php require "paginas/body.php"; ?>
+  <!-- CONTATO -->
+  <?php require "paginas/body.php"; ?>
 
 
   <div id="contato">
